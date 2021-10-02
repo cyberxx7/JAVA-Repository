e7d9af3d378261cdd80c56c260583933f43c65c7
Adeniyi Ademola <Adeniyiademola18@gmail.com>
2021-10-02 09:35:00 +0100

    file

--- src/ChapterTWO/Multiples.java
+++ src/ChapterTWO/Multiples.java
@@ -5,18 +5,18 @@ import java.util.Scanner;
 public class Multiples {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        int number1;
+        int numb1;
         int number2;
         System.out.println("Enter First Number: ");
-        number1 = input.nextInt();
+        numb1 = input.nextInt();
         System.out.println("Enter Second Number: ");
         number2 = input.nextInt();
-        if (number2 % number1 == 0) {
+        if (number2 % numb1 == 0) {
 
-            System.out.println(number1 + " is A " + "Multiple of " + number2);
+            System.out.println(numb1 + " is A " + "Multiple of " + number2);
         }
-        if (number2 % number1 >= 1 ) {
-            System.out.println(number1 + " is NOT a " + "Multiple of " + number2);
+        if (number2 % numb1 >= 1 ) {
+            System.out.println(numb1 + " is NOT a " + "Multiple of " + number2);
         }
     }
 }
